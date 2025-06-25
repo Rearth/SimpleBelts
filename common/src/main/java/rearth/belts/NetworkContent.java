93b4d8fab01796728eaafd44e8c4962196efe999
rearth <davidus2010@gmail.com>
2025-06-25 20:23:57 +0200

    Rename packages

---
+++ common/src/main/java/rearth/belts/NetworkContent.java
@@ -0,0 +1,11 @@
+package rearth.belts;
+
+public class NetworkContent {
+    
+    public static void init() {
+//        NetworkManager.registerReceiver(NetworkManager.Side.S2C, ChuteBlockEntity.ChuteDataPacket.FILTER_PACKET_ID, ChuteBlockEntity.ChuteDataPacket.PACKET_CODEC, (message, context) -> {
+//            ChuteBlockEntity.receivePacket(message, context.getPlayer().getWorld());
+//        });
+    }
+    
+}
